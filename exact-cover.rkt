2feb2bdb7f20d6e2762389f4257a90509d1e0039
Simon Holm Jensen <simonhj@cs.au.dk>
2013-06-05 21:02:56 +0200

    Added test case

--- exact-cover.rkt
+++ exact-cover.rkt
@@ -5,6 +5,8 @@
 ;; 
 ;; Author: Simon Holm Jensen <simon@hjensen.net>
 
+(require racket/generator)
+(require rackunit)
 (require "dancing-links.rkt")
 
 (struct exact-cover-instance (dl-rep 	;Dancing links representation
@@ -16,27 +18,41 @@
 				       [i (in-range (set-count universe))])
 			     (values i e))]
 	 [row->sets (for/hash ([s sets]
-			       [i (in-range (set-count sets))])
+			       [i (in-range 1 (add1 (set-count sets)))])
 		      (values i s))]
 	 [getter (lambda (x y)
-		   (if (equal? x 0)
+		   (if (equal? y 0)
 		       x		;The header
 		       (let* ([set (hash-ref row->sets y)]
 			      [elm (hash-ref row-idx->elements x)])
 			 (cond
 			  [(set-member? set elm) 1]
 			  [else 0]))))])
-    (displayln row-idx->elements)
-    (displayln row->sets)
-    (exact-cover-instance (build-dl getter (add1 (set-count sets)) (set-count universe)))))
+    (exact-cover-instance (build-dl getter (add1 (set-count sets)) (set-count universe)) row->sets row-idx->elements)))
 
-
-;; Tests
-(let ([A (set 1 4 7)]
-      [B (set 1 4)]
-      [C (set 4 5 7)]
-      [D (set 3 5 6)]
-      [E (set 2 3 6 7)]
-      [F (set 2 7)]
-      [universe (set 1 2 3 4 5 6 7)])
-  (make-exact-cover (set A B C D E F) universe))
+(define (solve-exact-cover problem)
+  (let ([dl (dlx (exact-cover-instance-dl-rep problem))]
+	[sets (exact-cover-instance-sets-mapping problem)]
+	[universe (exact-cover-instance-universe-mapping problem)])
+    (generator ()
+      (let loop ([val (dl)])
+	(cond
+	 [(void? val) (void)]
+	 [else (begin
+		 (yield (list->set (map (lambda (idx)
+			       (hash-ref sets idx)) val)))
+		 (loop (dl)))])))))
+    
+(module+ test
+  
+  (let* ([A (set 1 4 7)]
+	 [B (set 1 4)]
+	 [C (set 4 5 7)]
+	 [D (set 3 5 6)]
+	 [E (set 2 3 6 7)]
+	 [F (set 2 7)]
+	 [universe (set 1 2 3 4 5 6 7)]
+	 [ins (make-exact-cover (set A B C D E F) universe)]
+	 [sol (solve-exact-cover ins)])
+    (check-equal? (set B D F) (sol))))
+      
